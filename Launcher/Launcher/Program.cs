bc48c26b5a5c4f3c74dd118302c10a249f756e83
PizhikCoder <pashamalkovich@mail.ru>
2020-12-12 12:27:45 +0400

    *Fixes*

---
+++ Launcher/Launcher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Проверка разрядности операционной системы");
+            if (Environment.Is64BitOperatingSystem)
+            {
+                Console.WriteLine("Запускается клиент...");
+                Process.Start(Environment.CurrentDirectory + "client name");
+                Console.WriteLine("Клиент запущен");
+            }
+            else
+            {
+                Console.WriteLine("Запускается клиент...");
+                Process.Start(Environment.CurrentDirectory + "client name");
+                Console.WriteLine("Клиент запущен");
+            }
+            Environment.Exit(0);
+        }
+    }
+}
