78ba50b71efd751038e6db5025a08ec8431a2c9d
PizhikCoder <pashamalkovich@mail.ru>
2021-02-14 21:55:03 +0400

    *Little fixes*

--- Launcher/Launcher/Program.cs
+++ Launcher/Launcher/Program.cs
@@ -8,20 +8,23 @@ namespace Launcher
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Проверка разрядности операционной системы");
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + " Нажмите любую кнопку, чтобы начать...");
+            Console.ReadKey();
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + " Проверка разрядности операционной системы");
             if (Environment.Is64BitOperatingSystem)
             {
-                Console.WriteLine("Запускается клиент...");
-                Process.Start(Environment.CurrentDirectory + "client name");
-                Console.WriteLine("Клиент запущен");
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " Запускается x64 клиент...");
+                Process.Start(Environment.CurrentDirectory + "\\x64\\HTTP_WPF_Client-Project.exe");
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " Клиент запущен");
             }
             else
             {
-                Console.WriteLine("Запускается клиент...");
-                Process.Start(Environment.CurrentDirectory + "client name");
-                Console.WriteLine("Клиент запущен");
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " Запускается x86 клиент...");
+                Process.Start(Environment.CurrentDirectory + "\\x86\\HTTP_WPF_Client-Project.exe");
+                Console.WriteLine(DateTime.Now.ToShortTimeString() +  " Клиент запущен");
             }
-            Environment.Exit(0);
+            Console.WriteLine(DateTime.Now.ToShortTimeString() + " Нажмите любую кнопку, чтобы закрыть лаунчер...");
+            Console.ReadKey();
         }
     }
 }
